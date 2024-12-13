df1ab54626f7101a8ee49bdca0f4338a6b77806d
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-12-13 03:06:53 +0300

    +apply serialization plugin  ~cat dto change json name

--- composeApp/src/commonMain/kotlin/org/example/project/data/CatDTO.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/data/CatDTO.kt
@@ -1,8 +1,10 @@
 package org.example.project.data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CatDTO(
+    @SerialName("cat_name")
     val name: String
 )
