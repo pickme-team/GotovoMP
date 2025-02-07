a6cbd9bb310d7a1670a1d501c2f442911005746a
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-07 10:17:50 +0300

    fix step default naming

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/CreateRecipeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/CreateRecipeScreen.kt
@@ -1,25 +1,24 @@
 package org.example.project.presentation.screens
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Badge
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -38,19 +37,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.mohamedrejeb.richeditor.model.RichTextState
 import com.mohamedrejeb.richeditor.model.rememberRichTextState
-import com.mohamedrejeb.richeditor.ui.material3.RichTextEditor
-import com.mohamedrejeb.richeditor.ui.material3.RichTextEditorDefaults
-import kotlinx.coroutines.flow.MutableStateFlow
 import org.example.project.data.network.model.RecipeCreateRequest
 import org.example.project.presentation.components.StepTextField
-import org.example.project.presentation.util.StepState
 import org.example.project.viewModels.PersonalVM
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
@@ -75,12 +68,22 @@ fun CreateRecipeScreen(
     val listState = rememberLazyListState()
 
     Box(modifier = modifier.fillMaxSize()) {
-        LazyColumn(state = listState, verticalArrangement = Arrangement.spacedBy(16.dp), contentPadding = PaddingValues(16.dp)) {
+        LazyColumn(state = listState, contentPadding = PaddingValues(16.dp)) {
             item {
                 CenterAlignedTopAppBar(title = {
-                    Text(
-                        "Создать рецепт",
-                        style = MaterialTheme.typography.headlineSmall
+                    TextField(
+                        current.name,
+                        onValueChange = { current = current.copy(name = it) },
+                        placeholder = { Text("Название", style = MaterialTheme.typography.headlineSmall) },
+                        textStyle = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = MaterialTheme.shapes.medium,
+                        singleLine = true,
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent
+                        )
                     )
                 },
                     modifier = Modifier.padding(bottom = 16.dp),
@@ -93,22 +96,25 @@ fun CreateRecipeScreen(
                     })
             }
             item {
-                TextField(
-                    current.name,
-                    onValueChange = { current = current.copy(name = it) },
-                    placeholder = { Text("Название") },
-                    modifier = Modifier.fillMaxWidth(),
-                    shape = MaterialTheme.shapes.medium,
-                    singleLine = true,
-                    colors = TextFieldDefaults.colors(
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        disabledIndicatorColor = Color.Transparent
+                Row(modifier = Modifier.fillMaxWidth().clip(MaterialTheme.shapes.medium).clickable {  }.padding(vertical = 4.dp).padding(start = 16.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        "Ингридиенты",
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.padding(end = 8.dp)
                     )
-                )
+                    Badge(backgroundColor = MaterialTheme.colorScheme.primary) {
+                        Text(current.ingredients.size.toString(), color = MaterialTheme.colorScheme.onPrimary)
+                    }
+                    Spacer(
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(onClick = {}) { Icon(Icons.AutoMirrored.Default.KeyboardArrowRight, contentDescription = "to ingredients") }
+                }
             }
             itemsIndexed(steps) { index, state ->
                 StepTextField(
+                    modifier = Modifier.padding(top = 8.dp),
+                    index = index,
                     richTextState = state.second, headline = state.first to { steps[index] = state.copy(first = it) }, onRemove = { steps.removeAt(index) },
                     isFocused = focusIndex == index, changeFocus = {
                         if (it) { focusIndex = index }
@@ -117,7 +123,7 @@ fun CreateRecipeScreen(
             }
             item {
                 Row(
-                    modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
                     Button(
@@ -150,7 +156,7 @@ fun CreateRecipeScreen(
                 Button(
                     modifier = Modifier.fillMaxWidth().height(ButtonDefaults.MinHeight * 1.25f),
                     onClick = {
-                        val merged = steps.map { step -> step.first + "<br>" + step.second.toMarkdown() }.reduce { a, b -> "$a\n$b" }
+                        val merged = steps.mapIndexed { index, step -> step.first.ifBlank { "Шаг ${index+1}" } + "<br>" + step.second.toMarkdown() }.reduce { a, b -> "$a\n$b" }
                         viewModel.addRecipe(current.copy(text = merged), onCreated)
                     },
                     shape = MaterialTheme.shapes.medium
