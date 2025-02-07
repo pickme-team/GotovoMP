377acfb03ec83ab715599cb3e585a9c5c557a771
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-07 09:27:31 +0300

    split sections in view mode

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/ViewRecipeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/ViewRecipeScreen.kt
@@ -1,10 +1,14 @@
 package org.example.project.presentation.screens
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,8 +17,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.mohamedrejeb.richeditor.model.RichTextState
 import com.mohamedrejeb.richeditor.model.rememberRichTextState
 import com.mohamedrejeb.richeditor.ui.material3.RichText
 import org.example.project.viewModels.PersonalVM
@@ -23,7 +29,9 @@ import org.example.project.viewModels.PersonalVM
 @Composable
 fun ViewRecipeScreen(recipeId: Long, onBack: () -> Unit, modifier: Modifier = Modifier, viewModel: PersonalVM) {
     val recipe = viewModel.state.collectAsState().value.recipes.recipes.find { it.id == recipeId } ?: return
-    val richTextState = rememberRichTextState().setMarkdown(recipe.text)
+    val richTextState = rememberSaveable(recipe.text) { recipe.text.split('\n').map { md ->
+        RichTextState().apply { setMarkdown(md) }
+    } }
     LazyColumn(modifier = modifier, contentPadding = PaddingValues(16.dp)) {
         item {
             CenterAlignedTopAppBar(title = {
@@ -41,10 +49,11 @@ fun ViewRecipeScreen(recipeId: Long, onBack: () -> Unit, modifier: Modifier = Mo
                     }
                 })
         }
-        item {
-            RichText(
-                richTextState
-            )
+        items(richTextState, key = { it.toText() }) {
+            Card(modifier = Modifier.fillParentMaxWidth()) {
+                RichText(it, fontSize = MaterialTheme.typography.headlineSmall.fontSize, modifier = Modifier.padding(8.dp))
+            }
+            Spacer(modifier = Modifier.padding(bottom = 16.dp))
         }
     }
 }
