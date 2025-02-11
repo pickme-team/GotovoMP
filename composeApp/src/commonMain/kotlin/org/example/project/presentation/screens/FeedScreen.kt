394f8caa35cca4e797672cac020b10badbf17231
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-11 11:47:57 +0300

    feed ui rework, shared transitions

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/FeedScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/FeedScreen.kt
@@ -1,35 +1,34 @@
 package org.example.project.presentation.screens
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -45,24 +44,19 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
+import org.example.project.Const
+import org.example.project.data.network.model.Ingredient
 import org.example.project.domain.DomainError
 import org.example.project.presentation.util.Nav
 import org.example.project.presentation.util.bouncyClickable
 import org.example.project.viewModels.FeedScreenVM
 
-private val images = listOf(
-    "https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/8/86/Gnocchi_di_ricotta_burro_e_salvia.jpg",
-    "https://bakesbybrownsugar.com/wp-content/uploads/2019/11/Pecan-Cinnamon-Rolls-84-500x500.jpg",
-    "https://bestlah.sg/wp-content/uploads/2024/07/Best-Peranakan-Food-Singapore.jpeg"
-)
-
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
 @Composable
-fun FeedScreen(
+fun SharedTransitionScope.FeedScreen(
     navCtrl: NavHostController,
     feedScreenVM: FeedScreenVM = viewModel(),
+    animatedScope: AnimatedContentScope,
 ) {
     val uiState by feedScreenVM.state.collectAsState()
     PullToRefreshBox(
@@ -70,20 +64,18 @@ fun FeedScreen(
         isRefreshing = uiState.isLoading,
         onRefresh = feedScreenVM::loadRecipes,
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
+        LazyColumn(
             contentPadding = PaddingValues(24.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            item(span = { GridItemSpan(2) }) {
+            item {
                 Text(
                     "Рекомендации",
                     style = MaterialTheme.typography.headlineSmall,
                     modifier = Modifier.fillMaxWidth()
                 )
             }
-            item(span = { GridItemSpan(2) }) {
+            item {
                 uiState.error?.let {
                     Text(
                         when (it) {
@@ -96,28 +88,41 @@ fun FeedScreen(
                 }
             }
             items(uiState.recipes, key = { it.id }) { recipe ->
-                RecipeCard(recipe.name, images.random(), onClick = { navCtrl.navigate(Nav.VIEW.route + "/${recipe.id}") })
+                RecipeCard(
+                    title = recipe.name,
+                    ingredients = recipe.ingredients,
+                    imageUrl = Const.placeholderImages.random(),
+                    onClick = { navCtrl.navigate(Nav.VIEW.route + "/${recipe.id}") },
+                    modifier = Modifier.sharedBounds(
+                        rememberSharedContentState("view${recipe.id}",),
+                        animatedVisibilityScope = animatedScope
+                    )
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalSharedTransitionApi::class)
 @Composable
-fun RecipeCard(title: String, imageUrl: String, onClick: () -> Unit) =
-    Card(modifier = Modifier.fillMaxWidth().aspectRatio(0.8f).bouncyClickable(onClick = onClick)) {
+fun RecipeCard(title: String, ingredients: List<Ingredient>, imageUrl: String, onClick: () -> Unit, modifier: Modifier = Modifier) =
+    Card(modifier = modifier.fillMaxWidth().aspectRatio(0.8f).bouncyClickable(onClick = onClick, shrinkSize = 0.975f)) {
         var imageLoaded by rememberSaveable { mutableStateOf(false) }
+        val fadeColor =
+            MaterialTheme.colorScheme.run { if (!isSystemInDarkTheme()) onBackground else background }
+        val fadeInverse =
+            MaterialTheme.colorScheme.run { if (isSystemInDarkTheme()) onBackground else background }
         Box(
             modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomStart
         ) {
-            val fadeColor =
-                MaterialTheme.colorScheme.run { if (!isSystemInDarkTheme()) onBackground else background }
-            val fadeInverse =
-                MaterialTheme.colorScheme.run { if (isSystemInDarkTheme()) onBackground else background }
+            var lineCount by remember { mutableStateOf(1) }
             AsyncImage(modifier = Modifier.fillMaxSize().drawWithContent {
                 drawContent()
                 drawRect(
                     brush = Brush.verticalGradient(
-                        0f to Color.Transparent, 0.85f to fadeColor.copy(0.75f)
+                        0f to Color.Transparent,
+                        0.65f - .1f*lineCount to Color.Transparent,
+                        1f - .1f*lineCount  to fadeColor.copy(0.85f)
                     )
                 )
             },
@@ -125,16 +130,34 @@ fun RecipeCard(title: String, imageUrl: String, onClick: () -> Unit) =
                 contentDescription = title,
                 contentScale = ContentScale.Crop,
                 onSuccess = { imageLoaded = true })
-            Text(
-                text = title,
+
+            Column(
                 modifier = Modifier.padding(vertical = 16.dp, horizontal = 16.dp),
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 3,
-                style = MaterialTheme.typography.headlineLarge.copy(
-                    lineBreak = LineBreak.Paragraph,
-                    color = fadeInverse,
-                    fontWeight = FontWeight.Black
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = title,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 3,
+                    style = MaterialTheme.typography.displayMedium.copy(
+                        lineBreak = LineBreak.Paragraph,
+                        color = fadeInverse,
+                        fontWeight = FontWeight.Black
+                    ),
+                    onTextLayout = {
+                        lineCount = it.lineCount
+                    },
                 )
-            )
+                if (ingredients.isNotEmpty()) FlowRow(horizontalArrangement = Arrangement.spacedBy(8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                    ingredients.forEach {
+                        OutlinedCard(shape = MaterialTheme.shapes.large, colors = CardDefaults.outlinedCardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceContainer
+                        )) {
+                            Text(it.name, modifier = Modifier.padding(horizontal = 8.dp, vertical = 2.dp), fontWeight = FontWeight.Bold)
+                        }
+                    }
+                    Spacer(modifier = Modifier.padding(bottom = 16.dp))
+                }
+            }
         }
     }
