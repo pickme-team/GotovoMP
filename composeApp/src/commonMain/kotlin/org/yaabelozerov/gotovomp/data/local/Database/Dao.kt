0d25b477d32a8faa83aa514f6512e08a0648d6d4
mnwok <man.nikeman.21@gmail.com>
2025-08-05 19:55:52 +0300

    added a little

--- composeApp/src/commonMain/kotlin/org/yaabelozerov/gotovomp/data/local/Database/Dao.kt
+++ composeApp/src/commonMain/kotlin/org/yaabelozerov/gotovomp/data/local/Database/Dao.kt
@@ -4,8 +4,24 @@ import app.cash.sqldelight.db.SqlDriver
 import org.yaabelozerov.RecipeDatabase
 
 import org.yaabelozerov.gotovomp.RecipeQueries
+import org.yaabelozerov.gotovomp.data.network.model.RecipeCreateRequest
 
 class Dao(driver: SqlDriver) {
     val db = RecipeDatabase(driver)
     val queries: RecipeQueries = db.recipeQueries
+
+    fun saveRecipe(recipe: RecipeCreateRequest, fav: Boolean) {
+        recipe.run {
+            queries.insertRecipe(
+                null,
+                name,
+                "me", // TODO пиздец
+                text,
+                if (fav) 1 else 0
+            )
+        }
+    }
+    // TODO как добавлять теги, если я не знаю id рецепта
+    // аналогично с ингредиентами
+    // и как в целом делать базу данных, может есть варианты попроще
 }
